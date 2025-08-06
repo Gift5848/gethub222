a8b46f4af98911d05a26db98077cf6f368f9cc66
Your Name <ermias.sitotaw@crs.org>
2025-08-06 21:49:30 +0300

    Fix: ensure products is always an array in ProductList.js

--- frontend/src/components/ProductList.js
+++ frontend/src/components/ProductList.js
@@ -37,7 +37,7 @@ const ProductList = ({ filters = {}, searchTrigger, onAddToCart, onCartCountChan
     if (filters.year) params.year = filters.year;
     if (filters.search) params.search = filters.search;
     axios.get('/api/products', { params })
-      .then(res => setProducts(res.data))
+      .then(res => setProducts(Array.isArray(res.data) ? res.data : []))
       .catch(err => setError('Failed to load products.'))
       .finally(() => setLoading(false));
   }, [filters, searchTrigger]);
